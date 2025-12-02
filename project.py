d153e7483434b5b6843896e0b6d7d9e180e96676
rexatxyt <alirezarexa0@gmail.com>
2025-12-02 23:21:04 +0330

    add tamrin

--- project.py
+++
@@ -1,9 +0,0 @@
-Weight=int(input("enter your weight:"))
-if Weight > 100 :
-    print("your weight is more that our range")
-    
-elif Weight == 100 :
-    print("your weight is enough for range ")
-    
-else :
-    print("your weight is less than our range")
