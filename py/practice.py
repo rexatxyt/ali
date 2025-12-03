265e46497e027756087033f729bb44eecf500153
rexatxyt <alirezarexa0@gmail.com>
2025-12-03 19:19:10 +0330

    add tamrin

--- py/practice.py
+++ py/practice.py
@@ -1,10 +1,1 @@
-""" Limite=range(1,10)
-for i in Limite : 
-    print("*" * i)
-    for num in range(1,10) :
-        if num % 2 == 1:
-            for star in range (1,10):
-                print("*" * star)
-        else :
-            for star in range (11,0,-1) :
-                print("*" * star)  """
+print("wellcome")
